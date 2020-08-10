735db251d57ba14b92cf68aa142f141ac54b07bf
Ricardo Maricato <ricardo.maricato@hotmail.com>
2020-08-10 17:08:25 -0300

    Adicionando estrutura do Banck-end API's

---
+++ tc/src/main/java/br/com/rdevs/tc/TcApplication.java
@@ -0,0 +1,13 @@
+package br.com.rdevs.tc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcApplication.class, args);
+	}
+
+}
---
+++ tc/src/test/java/br/com/rdevs/tc/TcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rdevs.tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
